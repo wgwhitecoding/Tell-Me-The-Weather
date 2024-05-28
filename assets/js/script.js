333833fd30e7d881332e3171f8ea5e83e4367a4a
willcodinggod white <walidwhite@live.co.uk>
2024-05-28 14:30:16 +0000

    final no css added media query

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const map = L.map('map').setView([20, 0], 2); // Initialize map at a global view
+    const map = L.map('map').setView([20, 0], 2);  
 
                                                                                  // Add OpenStreetMap tiles
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
